85087fbb705ee88c8fd4564bf18bb3f43a0a6a8b
Manav Gupta <guptamanav077@gmail.com>
2020-08-06 20:02:52 +0100

    added a welcome popup modal on page load

---
+++ my_jquery_functions.js
@@ -0,0 +1,10 @@
+function popUp() {
+  // document.getElementById("hiddenModal").modal = 'show';
+  $('#hiddenModal').modal('show');
+}
+
+$(document).ready(function(){
+
+  // jQuery methods go here...
+
+});
