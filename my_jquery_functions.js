8f56fe1235b9e09eb0f8ac8597080946a3cd92ed
Manav Gupta <guptamanav077@gmail.com>
2020-08-07 21:45:11 +0100

    added cookies for username, doesnt work on chrome localhost

--- my_jquery_functions.js
+++ my_jquery_functions.js
@@ -1,7 +1,46 @@
+//This file with contain the functions that need to be loaded before body as this file is in the head.
+
 function popUp() { //have to have it outside the document ready because this is needed to load with the body not wait till body finishes loading
   // document.getElementById("hiddenModal").modal = 'show';
+  // var person = prompt("Please enter your full name:");
+  checkCookie();
   $('#hiddenModal').modal('show');
   startTime();
+  // document.cookie = "username="+person+"; expires=Thu, 18 Dec 2100 12:00:00 UTC";
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
 }
 
 $(document).ready(function(){
