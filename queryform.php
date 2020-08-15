dca4fcbdd962195249a74c58285c50888c73fc0d
Manav Gupta <guptamanav077@gmail.com>
2020-08-15 18:14:07 +0100

    got the form to be working, error 500 means that some problem in PHP code, values in SQL statement need to have single quotes around them in PHP

--- queryform.php
+++ queryform.php
@@ -1,5 +1,37 @@
+<!-- Use session to store in a variable either "thank you for submitting" or error, and display that variable in the html. -->
+
 <?php
-  
+  $servername = "localhost";
+  $username = "root";
+  $password = "password";
+  $dbname = "queries";
+
+  //Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  //Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $temp_firstname = $_GET["firstname"];
+  $temp_lastname = $_GET["lastname"];
+  $temp_email = $_GET["email"];
+  $temp_issue = $_GET["comment"];
+
+  $sql = "INSERT INTO queries (firstname, lastname, email, issue)
+  VALUES ('$temp_firstname','$temp_lastname','$temp_email','$temp_issue')";
+
+  $conn->query($sql);
+
+  //Comment out above line ($conn->query($sql);) if want to uncomment below 5 lines.
+
+  // if ($conn->query($sql) === TRUE) {
+  //   echo "New record created successfully";
+  // } else {
+  //   echo "Error: " . $sql . "<br>" . $conn->error;
+  // }
+
+  $conn->close();
 ?>
 
 <!-- Currently have to open PHP in a new page, in future can use AJAX to process PHP in the background -->
