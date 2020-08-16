fe6811090dcc331419bbde4f4fb6a3dbe39652fe
Manav Gupta <guptamanav077@gmail.com>
2020-08-16 19:48:58 +0100

    added image upload option

--- index.php
+++ index.php
@@ -284,6 +284,12 @@
       <button type="submit" name="append">Append</button>
     </form>
     <br>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+      Select image to upload:
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit" value="Upload Image" name="submit">
+    </form>
+    <br>
 
     <br>
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
